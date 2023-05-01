ba22fa7934e1500ab4c521d2d230243040433804
Artem Rockatez <razvorotkapr@gmail.com>
2023-05-01 15:36:21 +0200

    feat: add caps and backspace fethures

--- main.js
+++ main.js
@@ -16,3 +16,56 @@ document.querySelectorAll(".key").forEach(key => {
     input.focus();
   });
 });
+
+document.querySelectorAll(".capslock, .shift, .right-shift").forEach(key => {
+  key.addEventListener("click", event => {
+    event.target.classList.toggle("active");
+  });
+});
+
+document.querySelectorAll(".key").forEach(key => {
+  key.addEventListener("mousedown", event => {
+    key.classList.add("active");
+  });
+  key.addEventListener("mouseup", event => {
+    key.classList.remove("active");
+  });
+  key.addEventListener("mouseleave", event => {
+    key.classList.remove("active");
+  });
+});
+
+document.addEventListener("keydown", event => {
+  const keyChar = event.key;
+
+  if (keyChar === "Backspace") {
+    event.preventDefault();
+    input.value = input.value.slice(0, -1);
+  }
+
+   // iterate through virtual keyboard keys
+   document.querySelectorAll(".key").forEach(key => {
+    const keyValue = key.textContent;
+
+    // check if key value matches the pressed key
+    if (keyChar === keyValue) {
+      key.classList.add("active");
+    }
+  });
+
+    // toggle the Caps Lock key on/off
+    if (keyChar === "CapsLock") {
+      event.preventDefault();
+      isCapsLockOn = !isCapsLockOn;
+      document.querySelector(".capslock").classList.toggle("active");
+    }
+  
+    // update the input value with the pressed key
+    if (/[a-zA-Z0-9]/.test(keyChar)) {
+      if (isCapsLockOn) {
+        input.value += keyChar.toUpperCase();
+      } else {
+        input.value += keyChar.toLowerCase();
+      }
+    }
+  });
